2d788c220a27040874171bc7181c650f93553471
Thomas Zimmermann <thomas.zimmermann@signavio.com>
2020-12-20 20:12:37 +0100

    day 17

--- 17/cubes.ts
+++ 17/cubes.ts
@@ -1,17 +1,31 @@
 import { readFile } from "fs"
 
 type Cube = {
-  active: boolean
-  neighbors: Cube[]
-}
-
-// Indices are [z][y][x]
-type Grid = Cube[][][]
-
-type Point = {
   xIndex: number
   yIndex: number
   zIndex: number
+  wIndex: number
+  active: boolean
+}
+
+const print = (cubes: Cube[]): void => {
+  const { minX, maxX, minY, maxY, minZ, maxZ, minW, maxW } = getMinMaxValues(
+    cubes
+  )
+  for (let w = minW; w <= maxW; w++) {
+    for (let z = minZ; z <= maxZ; z++) {
+      console.log(`z=${z}, w=${w}`)
+      for (let y = minY; y <= maxY; y++) {
+        const symbols: string[] = []
+        for (let x = minX; x <= maxX; x++) {
+          const cube = findCube(x, y, z, 0, cubes)
+          symbols.push(cube.active ? "#" : ".")
+        }
+        console.log(symbols.join(""))
+      }
+      console.log("")
+    }
+  }
 }
 
 readFile("17/input.txt", "utf8", (err, data) => {
@@ -20,81 +34,195 @@ readFile("17/input.txt", "utf8", (err, data) => {
     return
   }
   const lines = data.split("\n")
-  const grid = initializeGrid(lines)
 
-  console.log(grid[0][0][1])
+  let activeCubes = initializeGrid(lines)
+  for (let cycle = 0; cycle < 6; cycle++) {
+    activeCubes = simulateCycle(activeCubes)
+  }
+  console.log(
+    "part 1 result: ",
+    activeCubes.filter((cube) => cube.active).length
+  )
+
+  activeCubes = initializeGrid(lines)
+  for (let cycle = 0; cycle < 6; cycle++) {
+    activeCubes = simulate4DCycle(activeCubes)
+  }
+  console.log(
+    "part 2 result: ",
+    activeCubes.filter((cube) => cube.active).length
+  )
 })
 
-const initializeGrid = (lines: string[]): Grid => {
-  const grid: Cube[][][] = []
+const simulateCycle = (activeCubes: Cube[]): Cube[] => {
+  const { minX, maxX, minY, maxY, minZ, maxZ } = getMinMaxValues(activeCubes)
+  const newActiveCubes: Cube[] = []
+  for (let z = minZ - 1; z <= maxZ + 1; z++) {
+    for (let y = minY - 1; y <= maxY + 1; y++) {
+      for (let x = minX - 1; x <= maxX + 1; x++) {
+        const currentCube: Cube = findCube(x, y, z, 0, activeCubes)
+        const activeNeighbors = getActiveNeighbors(currentCube, activeCubes)
+
+        let newActive
+        if (currentCube.active) {
+          newActive =
+            activeNeighbors.length === 2 || activeNeighbors.length === 3
+        } else {
+          newActive = activeNeighbors.length === 3
+        }
+        if (newActive) {
+          newActiveCubes.push({
+            ...currentCube,
+            active: newActive,
+          })
+        }
+      }
+    }
+  }
+  return newActiveCubes
+}
+
+const simulate4DCycle = (activeCubes: Cube[]): Cube[] => {
+  const { minX, maxX, minY, maxY, minZ, maxZ, minW, maxW } = getMinMaxValues(
+    activeCubes
+  )
+  const newActiveCubes: Cube[] = []
+  for (let w = minW - 1; w <= maxW + 1; w++) {
+    for (let z = minZ - 1; z <= maxZ + 1; z++) {
+      for (let y = minY - 1; y <= maxY + 1; y++) {
+        for (let x = minX - 1; x <= maxX + 1; x++) {
+          const currentCube: Cube = findCube(x, y, z, w, activeCubes)
+          const activeNeighbors = getActive4DNeighbors(currentCube, activeCubes)
+
+          let newActive
+          if (currentCube.active) {
+            newActive =
+              activeNeighbors.length === 2 || activeNeighbors.length === 3
+          } else {
+            newActive = activeNeighbors.length === 3
+          }
+          if (newActive) {
+            newActiveCubes.push({
+              ...currentCube,
+              active: newActive,
+            })
+          }
+        }
+      }
+    }
+  }
+  return newActiveCubes
+}
+
+const findCube = (
+  x: number,
+  y: number,
+  z: number,
+  w: number,
+  cubes: Cube[]
+): Cube =>
+  cubes.find(
+    (activeCube) =>
+      activeCube.xIndex === x &&
+      activeCube.yIndex === y &&
+      activeCube.zIndex === z &&
+      activeCube.wIndex === w
+  ) || {
+    xIndex: x,
+    yIndex: y,
+    zIndex: z,
+    wIndex: w,
+    active: false,
+  }
+
+const getActiveNeighbors = (cube: Cube, activeCubes: Cube[]): Cube[] => {
+  const activeNeighbors: Cube[] = []
+
+  neighborOffsets.forEach(([xOffset, yOffset, zOffset]) => {
+    const targetZ = cube.zIndex + zOffset
+    const targetY = cube.yIndex + yOffset
+    const targetX = cube.xIndex + xOffset
+    const neighbor = findCube(targetX, targetY, targetZ, 0, activeCubes)
+    if (neighbor.active) {
+      activeNeighbors.push(neighbor)
+    }
+  })
+
+  return activeNeighbors
+}
+
+const getActive4DNeighbors = (cube: Cube, activeCubes: Cube[]): Cube[] => {
+  const activeNeighbors: Cube[] = []
+
+  neighbor4DOffsets.forEach(([xOffset, yOffset, zOffset, wOffset]) => {
+    const targetZ = cube.zIndex + zOffset
+    const targetY = cube.yIndex + yOffset
+    const targetX = cube.xIndex + xOffset
+    const targetW = cube.wIndex + wOffset
+    const neighbor = findCube(targetX, targetY, targetZ, targetW, activeCubes)
+    if (neighbor.active) {
+      activeNeighbors.push(neighbor)
+    }
+  })
+
+  return activeNeighbors
+}
+
+const getMinMaxValues = (cubes: Cube[]) => {
+  let minX = Number.MAX_SAFE_INTEGER
+  let maxX = 0
+  let minY = Number.MAX_SAFE_INTEGER
+  let maxY = 0
+  let minZ = Number.MAX_SAFE_INTEGER
+  let maxZ = 0
+  let minW = Number.MAX_SAFE_INTEGER
+  let maxW = 0
+  cubes.forEach((cube) => {
+    minX = Math.min(minX, cube.xIndex)
+    minY = Math.min(minY, cube.yIndex)
+    minZ = Math.min(minZ, cube.zIndex)
+    minW = Math.min(minW, cube.wIndex)
+    maxX = Math.max(maxX, cube.xIndex)
+    maxY = Math.max(maxY, cube.yIndex)
+    maxZ = Math.max(maxZ, cube.zIndex)
+    maxW = Math.max(maxW, cube.wIndex)
+  })
+  return {
+    minX,
+    maxX,
+    minY,
+    maxY,
+    minZ,
+    maxZ,
+    minW,
+    maxW,
+  }
+}
+
+const initializeGrid = (lines: string[]): Cube[] => {
+  const activeCubes: Cube[] = []
   let zIndex = 0
-  grid[zIndex] = []
+  let wIndex = 0
   lines.forEach((line, yIndex) => {
     if (line.length === 0) {
       return
     }
-    grid[zIndex][yIndex] = []
     if (line.length >= 1) {
-      line.split("").forEach((symbol) => {
-        grid[zIndex][yIndex].push({
-          active: symbol === "#",
-          neighbors: [],
-        })
-      })
-    }
-  })
-
-  initializeNeighbors(grid)
-
-  for (let i = 0; i < 6; i++) {
-    simulateCycle(grid)
-  }
-
-  return grid
-}
-
-const simulateCycle = (grid: Grid): void => {
-  grid.forEach((zLayer, zIndex) => {
-    zLayer.forEach((row, yIndex) => {
-      row.forEach((cube, xIndex) => {
-        const activeNeighbors = cube.neighbors.filter(
-          (neighbor) => neighbor.active
-        ).length
-        if (cube.active) {
-          cube.active = activeNeighbors === 2 || activeNeighbors === 3
-        } else {
-          cube.active = activeNeighbors === 3
+      line.split("").forEach((symbol, xIndex) => {
+        if (symbol === "#") {
+          activeCubes.push({
+            xIndex,
+            yIndex,
+            zIndex,
+            wIndex,
+            active: true,
+          })
         }
       })
-    })
-  })
-}
-
-const initializeNeighbors = (grid: Grid): void => {
-  grid.forEach((zLayer, zIndex) => {
-    zLayer.forEach((row, yIndex) => {
-      row.forEach((cube, xIndex) => {
-        cube.neighbors = getNeighbors({ xIndex, yIndex, zIndex }, grid)
-      })
-    })
-  })
-}
-
-const getNeighbors = (
-  { xIndex, yIndex, zIndex }: Point,
-  grid: Grid
-): Cube[] => {
-  const neighbors: Cube[] = []
-  neighborOffsets.forEach(([xOffset, yOffset, zOffset]) => {
-    const neighbor =
-      grid[zIndex + zOffset] &&
-      grid[zIndex + zOffset][yIndex + yOffset] &&
-      grid[zIndex + zOffset][yIndex + yOffset][xIndex + xOffset]
-    if (neighbor) {
-      neighbors.push(neighbor)
     }
   })
-  return neighbors
+
+  return activeCubes
 }
 
 const neighborOffsets = [
@@ -113,7 +241,7 @@ const neighborOffsets = [
   [0, -1, +1],
   [+1, 0, -1],
   [-1, 0, +1],
-  [1, -1, -1],
+  [-1, -1, +1],
   [+1, +1, 0],
   [0, +1, +1],
   [+1, 0, +1],
@@ -124,4 +252,87 @@ const neighborOffsets = [
   [-1, +1, +1],
   [-1, +1, -1],
   [-1, -1, -1],
+]
+
+const neighbor4DOffsets = [
+  [0, 0, 0, -1],
+  [0, 0, 0, +1],
+  [0, 0, -1, 0],
+  [0, 0, -1, -1],
+  [0, 0, -1, +1],
+  [0, 0, +1, 0],
+  [0, 0, +1, -1],
+  [0, 0, +1, +1],
+  [0, -1, 0, 0],
+  [0, -1, 0, -1],
+  [0, -1, 0, +1],
+  [0, -1, -1, 0],
+  [0, -1, -1, -1],
+  [0, -1, -1, +1],
+  [0, -1, +1, 0],
+  [0, -1, +1, -1],
+  [0, -1, +1, +1],
+  [0, +1, 0, 0],
+  [0, +1, 0, -1],
+  [0, +1, 0, +1],
+  [0, +1, -1, 0],
+  [0, +1, -1, -1],
+  [0, +1, -1, +1],
+  [0, +1, +1, 0],
+  [0, +1, +1, -1],
+  [0, +1, +1, +1],
+  [-1, 0, 0, 0],
+  [-1, 0, 0, -1],
+  [-1, 0, 0, +1],
+  [-1, 0, -1, 0],
+  [-1, 0, -1, -1],
+  [-1, 0, -1, +1],
+  [-1, 0, +1, 0],
+  [-1, 0, +1, -1],
+  [-1, 0, +1, +1],
+  [-1, -1, 0, 0],
+  [-1, -1, 0, -1],
+  [-1, -1, 0, +1],
+  [-1, -1, -1, 0],
+  [-1, -1, -1, -1],
+  [-1, -1, -1, +1],
+  [-1, -1, +1, 0],
+  [-1, -1, +1, -1],
+  [-1, -1, +1, +1],
+  [-1, +1, 0, 0],
+  [-1, +1, 0, -1],
+  [-1, +1, 0, +1],
+  [-1, +1, -1, 0],
+  [-1, +1, -1, -1],
+  [-1, +1, -1, +1],
+  [-1, +1, +1, 0],
+  [-1, +1, +1, -1],
+  [-1, +1, +1, +1],
+  [+1, 0, 0, 0],
+  [+1, 0, 0, -1],
+  [+1, 0, 0, +1],
+  [+1, 0, -1, 0],
+  [+1, 0, -1, -1],
+  [+1, 0, -1, +1],
+  [+1, 0, +1, 0],
+  [+1, 0, +1, -1],
+  [+1, 0, +1, +1],
+  [+1, -1, 0, 0],
+  [+1, -1, 0, -1],
+  [+1, -1, 0, +1],
+  [+1, -1, -1, 0],
+  [+1, -1, -1, -1],
+  [+1, -1, -1, +1],
+  [+1, -1, +1, 0],
+  [+1, -1, +1, -1],
+  [+1, -1, +1, +1],
+  [+1, +1, 0, 0],
+  [+1, +1, 0, -1],
+  [+1, +1, 0, +1],
+  [+1, +1, -1, 0],
+  [+1, +1, -1, -1],
+  [+1, +1, -1, +1],
+  [+1, +1, +1, 0],
+  [+1, +1, +1, -1],
+  [+1, +1, +1, +1],
 ]
